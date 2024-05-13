d334ef6f71d42b81c09821197e5f32c27832561e
Yashodeepsonar <153747628+Yashodeepsonar@users.noreply.github.com>
2024-05-13 09:46:00 +0530

    Delete int.js

--- int.js
+++
@@ -1,6 +0,0 @@
-// const validator = require('validator');
-// const colors = require('colors');
-
-// const res= validator.isEmail('foo@barm.com');
-// console.log(res ? colors.green.inverse(res) : colors.red.inverse(res));
-// console.log(colors.rainbow('Yash sonar')); 
